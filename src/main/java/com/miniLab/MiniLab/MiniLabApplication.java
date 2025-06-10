f5db2964adc3bb14da740eb240ec2b1e40623b9b
Enmanuel <end199615@gmail.com>
2025-06-10 10:41:04 -0500

    first commit

---
+++ src/main/java/com/miniLab/MiniLab/MiniLabApplication.java
@@ -0,0 +1,13 @@
+package com.miniLab.MiniLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miniLab/MiniLab/MiniLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miniLab.MiniLab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
