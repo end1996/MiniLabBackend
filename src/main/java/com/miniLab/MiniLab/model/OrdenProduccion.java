2e7a53e6ab6feb3a3e88538fa9bafa30638a9f93
Enmanuel <end199615@gmail.com>
2025-06-11 11:30:56 -0500

    Empleado, OrdenProduccion and Usuario Model

--- src/main/java/com/miniLab/MiniLab/model/OrdenProduccion.java
+++ src/main/java/com/miniLab/MiniLab/model/OrdenProduccion.java
@@ -1,4 +1,25 @@
 package com.miniLab.MiniLab.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Date;
+
+@Data
+@Entity
+@NoArgsConstructor
 public class OrdenProduccion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private int cantidad_cuadros;
+    private Date fecha_inicio;
+    private boolean estado;
+    private String observaciones;
+
+    @ManyToOne
+    @JoinColumn(name = "id_empleado", referencedColumnName = "id")
+    private Empleado empleado;
 }
--- src/main/java/com/miniLab/MiniLab/model/Usuario.java
+++ src/main/java/com/miniLab/MiniLab/model/Usuario.java
@@ -1,4 +1,24 @@
 package com.miniLab.MiniLab.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table (name="Usuario")
+@NoArgsConstructor
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    private String rol;
+    private String usuario;
+    private String clave;
+
+    @OneToOne
+    @JoinColumn(name= "empleado_id", referencedColumnName = "id")
+    private Empleado empleado;
 }
